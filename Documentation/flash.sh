cab5e2b4a59a5c40d04e875d71321887643755f8
daniel.paredes <daniel.paredes@q-loud.de>
2021-12-20 11:11:02 +0100

    feat: script to flash throught usb port

--- Documentation/flash.sh
+++ Documentation/flash.sh
@@ -3,6 +3,6 @@
 PATH_PROGRAMMER="/home/me/STMicroelectronics/STM32Cube/STM32CubeProgrammer/bin"
 PROGRAMMER="${PATH_PROGRAMMER}/STM32_Programmer_CLI"
 
-echo "sudo ${PROGRAMMER} -c port=usb1 -w $1 0x08000000"
+sudo ${PROGRAMMER} -c port=usb1 -w "$1" 0x08000000
 
 # sudo ./STM32_Programmer_CLI -c port=usb1 -w  ~/Downloads/temp/STM32CubeFunctionPack_DATALOG1_V1.3.0/Projects/STM32L4R9ZI-STWIN/Applications/HSDatalog/Binary/HSDatalog.bin 0x08000000 
