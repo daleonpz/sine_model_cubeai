9d79cd3f3a6fd0272533eb20767fcadbdc3e9722
daniel.paredes <daniel.paredes@q-loud.de>
2022-01-03 12:48:37 +0100

    rfac: plot value of sensors overtime

--- stwinkt_datacollector/plot_acc.py
+++ stwinkt_datacollector/plot_acc.py
@@ -1,158 +1,121 @@
-#!/bin/python3
+#!/usr/bin/env python
 
-
-# import re
-# import numpy as np
-# import matplotlib
-# matplotlib.use("TkAgg")
-# import matplotlib.pyplot as plt
-# import math
-# import serial
-# 
-# ser = serial.Serial('/dev/ttyACM0', 115200)
-# ser.flushInput()
-# 
-# plot_window = 200
-# x_var = np.array(np.zeros([plot_window]))
-# y_var = np.array(np.zeros([plot_window]))
-# z_var = np.array(np.zeros([plot_window]))
-# 
-# plt.ion()
-# fig, ax = plt.subplots()
-# line, = ax.plot(y_var)
-# 
-
-# while True:
-#     with serial.Serial('/dev/ttyACM0', 115200, timeout=10) as ser:
-#         sensor_rawdata = ser.readline()  
-#         sensor_rawdata = sensor_rawdata.decode('utf-8')
-#         sensor_data = [float(s) for s in re.findall(r'-?\d+\.?\d*', sensor_rawdata)]
-#         x_data = sensor_data[0::3]
-#         y_data = sensor_data[1::3]
-#         z_data = sensor_data[2::3]
-#         # [ MES_0... MES_K] , und MES_i = x,y,z in diesem Fall 
-#         print(sensor_data)
-#         print(x_data)
-#         print(y_data)
-#         print(z_data)
-
-# # Plot the data in each partition in different colors:
-# plt.plot(x_train, y_train, 'b.', label="Train")
-# plt.plot(x_test, y_test, 'r.', label="Test")
-# plt.plot(x_val, y_val, 'y.', label="Validate")
-# plt.legend()
-# plt.show()
-
-
+from threading import Thread
 import serial
-import numpy as np
-from matplotlib import pyplot as plt
-from threading import Thread,Event
-import time
 import re
+import time
+import collections
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import struct
+# import pandas as pd
 
-#A simple ring buffer to keep the plotting data in
-class RingBuffer(object):
-    def __init__(self,size):
-        self.size = size
-        self._size = 2*size
-        self._buffer = np.zeros(self._size)
-        self._idx = 0
 
-    def insert(self,val):
-        idx = self._idx%self.size
-        self._buffer[idx] = val
-        self._buffer[idx+self.size] = val
-        self._idx+=1
-        if self._idx > self.size:
-            self._idx-=self.size
+class serialPlot:
+    def __init__(self, serialPort = '/dev/ttyACM0', serialBaud = 115200, plotLength = 100, dataNumBytes = 1):
+        self.port = serialPort
+        self.baud = serialBaud
+        self.plotMaxLength = plotLength
+        self.dataNumBytes = dataNumBytes
+        self.rawData = bytearray(dataNumBytes)
+        self.data_x = collections.deque([0] * plotLength, maxlen=plotLength)
+        self.data_y = collections.deque([0] * plotLength, maxlen=plotLength)
+        self.data_z = collections.deque([0] * plotLength, maxlen=plotLength)
+        self.isRun = True
+        self.isReceiving = False
+        self.thread = None
+        self.plotTimer = 0
+        self.previousTimer = 0
+        # self.csvData = []
 
-    def get(self):
-        start_idx = (self._idx)%self.size
-        end_idx = start_idx+self.size
-        return self._buffer[start_idx:end_idx]
+        print('Trying to connect to: ' + str(serialPort) + ' at ' + str(serialBaud) + ' BAUD.')
+        try:
+            self.serialConnection = serial.Serial(serialPort, serialBaud, timeout=4)
+            print('Connected to ' + str(serialPort) + ' at ' + str(serialBaud) + ' BAUD.')
+        except:
+            print("Failed to connect with " + str(serialPort) + ' at ' + str(serialBaud) + ' BAUD.')
 
-#A thread to handle plotting and reading data from the serial port
-class Plotter(Thread):
-#     def __init__(self,stream,stop,pause):
-    def __init__(self,stream):
-        Thread.__init__(self)
-        self.stream = stream
-#         plt.ion()
-#         plt.show()
-#         self.fig = plt.figure()
-#         self.ax = plt.axes()
-#         self.size = 100
-#         self.xdata = np.arange(self.size)
-#         self.ydata = RingBuffer(self.size)
-#         self.line, = self.ax.plot(self.ydata.get())
-#         plt.ylim([24,29])                                                  
-#         self.stop = stop
-#         self.pause = pause
+    def readSerialStart(self):
+        if self.thread == None:
+            self.thread = Thread(target=self.backgroundThread)
+            self.thread.start()
+            # Block till we start receiving values
+            while self.isReceiving != True:
+                time.sleep(0.1)
 
-#The main loop of the thread, invoked with .start()
-    def run(self):
-#         while not self.stop.is_set():
-#             if self.pause.is_set():
-#                 continue
-#             raw_data = self.stream.readline().rstrip()                     
-        while True:
-            raw_data = self.stream.readline().decode('utf-8')
-#         sensor_rawdata = sensor_rawdata.decode('utf-8')
-#         sensor_data = [float(s) for s in re.findall(r'-?\d+\.?\d*', sensor_rawdata)]
-#         x_data = sensor_data[0::3]
-#         y_data = sensor_data[1::3]
-#         z_data = sensor_data[2::3]
-#         # [ MES_0... MES_K] , und MES_i = x,y,z in diesem Fall 
-#         print(sensor_data)
-#         print(x_data)
-#         print(y_data)
-#         print(z_data)
-#             if len(raw_data) <= 6:
-            try:
-#                     data = float(raw_data)
-                data = [float(s) for s in re.findall(r'-?\d+\.?\d*', raw_data)]
-                data = data[0::3]
-                print(data)
-            except ValueError:
-                print ("Not a float")
-#             else:
-#                     if data <= 100:
-#                 self.ydata.insert(data)
-#                 self.line.set_xdata(self.xdata)
-#                 self.line.set_ydata(self.ydata.get())
-#                 plt.draw()
+    def getSerialData(self, frame, lines_x, lines_y, lines_z, lineValueText, timeText):
+        currentTimer = time.perf_counter()
+        self.plotTimer = int((currentTimer - self.previousTimer) * 1000)     # the first reading will be erroneous
+        self.previousTimer = currentTimer
+        timeText.set_text('Plot Interval = ' + str(self.plotTimer) + 'ms')
+#         value,  = struct.unpack('f', self.rawData)    # use 'h' for a 2 byte integer
+        value = self.rawData
+        self.data_x.append(value[0])    # we get the latest data point and append it to our array
+        self.data_y.append(value[1])    # we get the latest data point and append it to our array
+        self.data_z.append(value[2])    # we get the latest data point and append it to our array
+        lines_x.set_data(range(self.plotMaxLength), self.data_x)
+        lines_y.set_data(range(self.plotMaxLength), self.data_y)
+        lines_z.set_data(range(self.plotMaxLength), self.data_z)
+#         lineValueText.set_text('[' + lineLabel + '] = ' + str(value[0]))
+        # self.csvData.append(self.data[-1])
 
-        #sleep statement to reduce processor load 
-        time.sleep(1) 
+    def backgroundThread(self):    # retrieve data
+        time.sleep(1.0)  # give some buffer time for retrieving data
+        self.serialConnection.reset_input_buffer()
 
-#this is implemented to facilitate use through something like Ipython easily
-class Controller(object):
-    def __init__(self):
-        #These events control execution of the thread main loop
-#         self.stop = Event()
-#         self.pause = Event()
-        self.stream = serial.Serial('/dev/ttyACM0', 115200)
-#         self.plotter_thread = Plotter(self.stream,self.stop,self.pause)
-        self.plotter_thread = Plotter(self.stream)
-        self.plotter_thread.start()
+        while (self.isRun):
+#             self.serialConnection.readinto(self.rawData)
+#             self.isReceiving = True
+
+            raw_data = self.serialConnection.readline().decode('utf-8')
+            data = [float(s) for s in re.findall(r'-?\d+\.?\d*', raw_data)]
+#             data = data[0::3]
+#             data = data[0] # just one value
+            self.rawData = data
+            self.isReceiving = True
+            print(self.rawData)
+
+    def close(self):
+        self.isRun = False
+        self.thread.join()
+        self.serialConnection.close()
+        print('Disconnected...')
+        # df = pd.DataFrame(self.csvData)
+        # df.to_csv('/home/rikisenia/Desktop/data.csv')
+
 
 def main():
-    con = Controller()
-    #Loop over terminal input
-    #possible commands are stop, pause and unpause
-#     while True:
-#         command = input("cmd >>>")
-#         if command == "stop":
-#             con.stop.set()
-#             con.plotter_thread.join()
-#             break
-#         elif command == "pause":
-#             con.pause.set()
-#         elif command == "unpause":
-#             con.pause.clear()
-#         else:
-#             print("unrecognised command")
-# 
-if __name__ == "__main__":
+    portName = '/dev/ttyACM0'
+    baudRate = 115200 
+    maxPlotLength = 100
+    dataNumBytes = 1        # number of bytes of 1 data point
+    s = serialPlot(portName, baudRate, maxPlotLength, dataNumBytes)   # initializes all required variables
+    s.readSerialStart()                                               # starts background thread
+
+    # plotting starts below
+    pltInterval = 50    # Period at which the plot animation updates [ms]
+    xmin = 0
+    xmax = maxPlotLength
+    ymin = -(1000)
+    ymax = 1000
+    fig = plt.figure()
+    ax = plt.axes(xlim=(xmin, xmax), ylim=(float(ymin - (ymax - ymin) / 10), float(ymax + (ymax - ymin) / 10)))
+    ax.set_title('Analog Read')
+    ax.set_xlabel("time")
+    ax.set_ylabel("AnalogRead Value")
+
+    timeText = ax.text(0.50, 0.95, '', transform=ax.transAxes)
+    lines_x = ax.plot([], [], label="X")[0]
+    lines_y = ax.plot([], [], label="Y")[0]
+    lines_z = ax.plot([], [], label="Z")[0]
+    lineValueText = ax.text(0.50, 0.90, '', transform=ax.transAxes)
+    anim = animation.FuncAnimation(fig, s.getSerialData, fargs=(lines_x, lines_y, lines_z, lineValueText, timeText), interval=pltInterval)    # fargs has to be a tuple
+
+    plt.legend(loc="upper left")
+    plt.show()
+
+    s.close()
+
+
+if __name__ == '__main__':
     main()
